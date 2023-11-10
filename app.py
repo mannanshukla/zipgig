e712e2abdfe4a0275e35c939297675190e5fb342
mannan <mannanshukla@icloud.com>
2023-11-10 05:19:18 -0500

    added a bunch of stuff, but for now the template reflects whats on the json which is a start lol

--- app.py
+++ app.py
@@ -13,14 +13,14 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
-f = open('my_data.json')
+f = open('data.json')
 
-my_data = json.load(f)
+data = json.load(f)
 
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
-    return templates.TemplateResponse("item.html", {"request": request, "my_data": my_data})
+    return templates.TemplateResponse("index.html", {"request": request, "data": data})
 
 if __name__ == "__main__":
     uvicorn.run(app)
